6a575c2aba31e44534c140941d1c37e770670b7c
kand193 <qkrdudwls99@gmail.com>
2019-05-07 17:30:46 +0900

    Implement simple server with typescript

--- src/server.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { renderToString } from "react-dom/server";
-
-const app = express();
-
-app.listen(1599, () => {
-  console.log("HELLO WORLD");
-});
---
+++ src/server.tsx
@@ -0,0 +1,30 @@
+import express from "express";
+import fs from "fs";
+import path from "path";
+import React from "react";
+import { renderToString } from "react-dom/server";
+import { StaticRouter } from "react-router-dom";
+
+import App from "./app/App";
+
+const app = express();
+
+app.get("*", (req, res) => {
+  const markup = renderToString(
+    <StaticRouter>
+      <App />
+    </StaticRouter>
+  );
+
+  const indexFile = path.resolve('./src/template.html');
+  fs.readFile(indexFile, "utf8", (err, data) =>
+    res.send(data.replace(
+      `<div id="app"></div>`,
+      `<div id="app">${markup}</div>`
+    ))
+  );
+})
+
+app.listen(1599, () => {
+  console.log("Server listening on port 1599");
+});
